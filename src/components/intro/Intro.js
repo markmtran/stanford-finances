a24c52b0839686844d4c4c4a643c4f311b5d75d9
Mark Tran <markmtran1@gmail.com>
2021-04-09 00:05:29 -0400

    reformatted header bar, added intro pic

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,4 +1,4 @@
-import { AppBar, Typography } from "@material-ui/core";
+import { AppBar, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
@@ -7,6 +7,14 @@ const useStyles = makeStyles({
   },
   headerFont: {
     fontFamily: "Source Serif Pro, serif"
+  },
+  titleText: {
+    fontFamily: "Source Sans Pro, sans",
+    color: "white",
+    fontWeight: 100
+  },
+  introDiv: {
+    backgroundImage: 'url("https://www-media.stanford.edu/wp-content/uploads/2017/06/20191526/stanford-share.jpg")'
   }
 });
 
@@ -14,11 +22,22 @@ const Intro = () => {
   const classes = useStyles();
 
   return(
-    <AppBar className={classes.appBarStyle} position="static">
-      <Typography className={classes.headerFont} variant="h6" color="inherit">
-        Stanford University
-      </Typography>
-    </AppBar>
+    <div>
+      <AppBar className={classes.appBarStyle} position="static">
+        <Grid container>
+          <Grid item xs="1"/>
+          <Grid item xs="11">
+            <Typography className={classes.headerFont} variant="h6" color="inherit">
+              Stanford | Finances
+            </Typography>
+          </Grid>
+        </Grid>
+      </AppBar>
+      <div>
+        <Typography className={classes.titleText} variant="h3">Understanding Stanford's Finances</Typography>
+      </div>
+      <img src="https://www-media.stanford.edu/wp-content/uploads/2017/06/20191526/stanford-share.jpg" alt="Stanford University at Night" width="100%"/>
+    </div>
   )
 }
 
