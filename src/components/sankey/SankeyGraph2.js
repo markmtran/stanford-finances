822f0d7274bceb5694c23c527df36fed918ab7c6
Mark Tran <markmtran1@gmail.com>
2021-04-23 16:00:24 -0400

    added customizable tooltips to the sankey, got animations working

--- src/components/sankey/SankeyGraph2.js
+++ src/components/sankey/SankeyGraph2.js
@@ -1,7 +1,20 @@
 import { ResponsiveSankey } from "@nivo/sankey";
 import { Grid, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  tipHeader: {
+    fontWeight: "bold"
+  },
+  tipDesc: {
+
+  },
+  nodeStyle: {
+    fontWeight: "bold"
+  }
+})
+
 const SankeyGraph2 = () => {
+  const classes = useStyles();
   const data = {
     "nodes": [
       {
@@ -45,7 +58,7 @@ const SankeyGraph2 = () => {
       {
         "source": "Price per Student",
         "target": "",
-        "value": 77643.67
+        "value": 78165.00
       },
       {
         "source": "Stanford Subsidy",
@@ -69,13 +82,13 @@ const SankeyGraph2 = () => {
       },
       {
         "source": "",
-        "target": "Library",
-        "value": 7479.56
+        "target": "Facilities O&M",
+        "value": 7609.43
       },
       {
         "source": "",
-        "target": "Facilities O&M",
-        "value": 7609.43
+        "target": "Library",
+        "value": 7479.56
       },
       {
         "source": "",
@@ -99,12 +112,76 @@ const SankeyGraph2 = () => {
       }
     ]
   }
+  function getLinkTooltip(node) {
+    let desc = "";
+    let val = "";
+    switch(node.source.label) {
+      case "Price per Student":
+        desc = "pps description";
+        val = "$78,165.00"
+        break;
+      case "Stanford Subsidy":
+        desc = "ss description";
+        val = "$77,643.67"
+        break;
+      default:
+        break;
+    }
+    switch(node.target.label) {
+      case "Instruct/Dept Research":
+        desc = "I/D research description";
+        val = "$69,624.43"
+        break;
+      case "FinAid Costs":
+        desc = "FA description";
+        val = "$31,755.31"
+        break;
+      case "Dep't Administration":
+        desc = "DA description";
+        val = "$19,275.27"
+        break;
+      case "Library":
+        desc = "library description";
+        val = "$7,479.56"
+        break;
+      case "Facilities O&M":
+        desc = "fom description";
+        val = "$7,609.43"
+        break;
+      case "Student Services":
+        desc = "stuserv description";
+        val = "$7,082.68"
+        break;
+      case "Administration (G&A)":
+        desc = "admin g&a description";
+        val = "$5,088.03"
+        break;
+      case "Depreciation":
+        desc = "dep description";
+        val = "$4,740.75"
+        break;
+      case "Institutional/Community":
+        desc = "I/C description";
+        val = "$3,153.21"
+        break;
+      default:
+        break;
+    }
+    
+    return (
+      <div>
+        <span className={classes.tipHeader}>{val}</span><br />
+        <span className={classes.tipDesc}>{desc}</span>
+      </div>
+    )
+  }
   return(
     <Grid container justify="center">
       <Grid item xs={1} />
-      <Grid item xs={10} style={{height: "750px"}}>
+      <Grid item xs={10} style={{height: "700px"}}>
         <ResponsiveSankey
           data={data}
+          sort="descending"
           margin={{ top: 20, bottom: 80, left: 140, right: 140}}
           align="justify"
           colors={{ scheme: 'category10' }}
@@ -114,6 +191,11 @@ const SankeyGraph2 = () => {
           nodeSpacing={20}
           nodeBorderWidth={0}
           nodeBorderColor={{ from: 'color', modifiers: [ [ 'darker', 0.8 ] ] }}
+          nodeTooltip={node =>
+            (node.label === "") ? <span className={classes.nodeStyle}>Student</span> :
+            <span className={classes.nodeStyle}>{node.label}</span>
+          }
+          linkTooltip={node => getLinkTooltip(node)}
           linkOpacity={0.5}
           linkHoverOpacity={0.7}
           linkHoverOthersOpacity={0.1}
